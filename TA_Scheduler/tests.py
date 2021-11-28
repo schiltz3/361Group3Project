3d50fbc983bf564944afbdb249a6ca82fa5d9a6b
lilylevrault <levrault@uwm.edu>
2021-11-28 11:12:53 -0600

    what i have so far, none of them are done yet

--- TA_Scheduler/tests.py
+++ TA_Scheduler/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Account
 
-# Create your tests here.
+
+class NewAccountTest(TestCase):
+    def setUp(self):
+        self.client = Client()
+        m = Account(username="user", password="pass")
+        m.save()
+        # do I have to navigate to createaccounts page or can I just start there??
+        # self.client.post("/login.html", {"name": "user", "password": "pass"})
+        # self.client.post("/home.html", select button? to go to createaccounts)
+
+    def test_valid(self):
+        response = self.client.post("/createaccounts.html", {"username": "new", "password": "password"})
+        self.assertEqual("account 'new' created", response.context["message"], "confirmation message not given")
+
+    def test_inDatabase(self):
+        self.client.post("/createaccounts.html", {"username": "new", "password": "password"})
+        self.assertEqual(Account(username="new", password="password"), Account.objects.get(username="new"))
+
+    # how would i test for when there's no username/password? (they're required in the html)
+
+    def test_usernameTaken(self):
+        response = self.client.post("/createaccounts.html", {"username": "user", "password": "password"})
+        self.assertEqual("username 'user' is already in use", response.context["message"], "username already taken")
+
+    def test_noNewDuplicate(self):
+        self.client.post("/createaccounts.html", {"username": "user", "password": "password"})
+        #make sure casting to dict and getting length is correct
+        self.assertEqual(1, dict(Account.objects.filter(username="user")).length, "duplicate user created")
+
--- TA_Scheduler/views.py
+++ TA_Scheduler/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import Account
 
-# Create your views here.
+
+class CreateAccounts(View):
+    def get(self, request):
+        return render(request, "createaccounts.html", {})
+
+    def post(self, request):
+        userExists = True
+        username=request.POST['username']
+        try:
+            m = Account.objects.get(username=username)
+        except:
+            userExists = False
+        if userExists:
+            return render(request, "createaccounts.html", {"message": "username '"+username+"' is already in use"})
+        else:
+            m = Account(username=username, password=request.POST['password'])
+            m.save()
+            # should I stay here or go somewhere else?
+            return render(request, "createaccounts.html", {"message": "account '"+username+"' created"})
