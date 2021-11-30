7b38c5a5e90745798d26daa2e387dcf6853dc733
John Schiltz <johnschiltz2000@gmail.com>
2021-11-30 00:18:33 -0600

    broke code

--- TA_Scheduler/views/course/create.py
+++ TA_Scheduler/views/course/create.py
@@ -2,27 +2,30 @@ from django.shortcuts import render
 from TA_Scheduler.models import Account
 from django.views import View
 from TA_Scheduler.utilities.AccountUtil import AccountUtil
-from TA_Scheduler.utilities.CourseUtil import CourseUtil 
+from TA_Scheduler.utilities.CourseUtil import CourseUtil
+
 
 class CreateCourse(View):
 
     # this is called when the user opens the page, the course/create.html
     def get(self, request):
-        return render(request, 'course/create.html', {'message' : '', 'instructors' : AccountUtil.getInstructors()})
+        return render(request, 'course/create.html', {'message': '', 'instructors': AccountUtil.getInstructors()})
 
     # this is called when the user clicks submit.
-    def post(self, request):
+    def post(self, request)
 
         # if user is anonymous or not admin, show error
         if request.user.is_anonymous or request.user.account.authority == 0:
-            return render(request, 'course/create.html', {'message' : 'User is not authorized to create the course.', 'instructors' : AccountUtil.getInstructors()})
+            return render(request, 'course/create.html', {'message': 'User is not authorized to create the course.',
+                                                          'instructors': AccountUtil.getInstructors()})
 
         name = request.POST['name']
         description = request.POST['description']
 
         # if name is blank show error
         if not name:
-            return render(request, 'course/create.html', {'message' : 'Course name must not be blank.', 'instructors' : AccountUtil.getInstructors()})
+            return render(request, 'course/create.html',
+                          {'message': 'Course name must not be blank.', 'instructors': AccountUtil.getInstructors()})
 
         instructor = request.POST['instructor']
 
@@ -30,4 +33,4 @@ class CreateCourse(View):
         # TODO need to confirm this works when users can be created
         CourseUtil.createCourse(name, description, instructor)
 
-        return render(request, 'course/create.html', {'message' : 'Course created.'})
+        return render(request, 'course/create.html', {'message': 'Course created.'})
