5f155ebbb6f4f73c07aabb958acc54da9bef4f31
John Schiltz <45466247+schiltz3@users.noreply.github.com>
2021-11-30 19:57:50 +0000

    Automated formating

--- CS361Group3Project/urls.py
+++ CS361Group3Project/urls.py
@@ -20,5 +20,5 @@ from TA_Scheduler.views.course.create import CreateCourse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("course/create/", CreateCourse.as_view(), name='create-course')
+    path("course/create/", CreateCourse.as_view(), name="create-course"),
 ]
