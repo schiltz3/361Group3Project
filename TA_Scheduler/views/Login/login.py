5bf084133829ae63220a6afdea90319f7c2bb28f
TouKoua <toukoua@uwm.edu>
2021-12-01 00:33:32 -0600

    created a new folder to hold my login views file and wrote a basic views for login

--- TA_Scheduler/views/Login/login.py
+++ TA_Scheduler/views/Login/login.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import View
+from TA_Scheduler.models import Account
 
-# Create your views here.
+
+class Login(View):
+
+    def get(self, request):
+        return render(request, "login/login.html", {})
+
+    def post(self, request):
+        invalid = False
+        try:
+            user = Account.objects.get(username=request.POST('username'))
+            invalidPassword = (user.password() != request.POST('password'))
+        except:
+            invalid = True
+        if invalid:
+            render(request, "login/login.html", {"error: Invalid username"})
+        elif invalidPassword:
+            render(request, "login/login.html", {"error: Invalid password"})
+        else:
+            request.session["username"] = user.username
+            return redirect("login/home.html")
