30b7db2eea1f5e18a4428d84ae38c33a9e2fa8fb
lilylevrault <levrault@uwm.edu>
2021-12-05 11:13:00 -0600

    update: merged master into branch

--- TA_Scheduler/AcceptanceTests/tests_createaccounts.py
+++ TA_Scheduler/AcceptanceTests/tests_createaccounts.py
@@ -12,26 +12,25 @@ class NewAccountTest(TestCase):
         # self.client.post("/home.html", select button? to go to createaccounts)
 
     def test_valid(self):
-        response = self.client.post("/createaccounts.html", {"username": "new", "password": "password"})
+        response = self.client.post("account/create.html", {"username": "new", "password": "password"})
         self.assertEqual("account 'new' created", response.context["message"], "confirmation message not given")
 
     def test_inDatabase(self):
-        self.client.post("/createaccounts.html", {"username": "new", "password": "password"})
+        self.client.post("account/create.html", {"username": "new", "password": "password"})
         self.assertNotEqual(None, AccountUtil.getAccountByUsername("new"))
 
     def test_noUsername(self):
-        self.client.post("/createaccounts.html", {"username": "", "password": "password"})
+        self.client.post("account/create.html", {"username": "", "password": "password"})
         self.assertEqual(1, Account.objects.all().count(), "new user created even though no username entered")
 
     def test_noPassword(self):
-        self.client.post("/createaccounts.html", {"username": "new", "password": ""})
+        self.client.post("account/create.html", {"username": "new", "password": ""})
         self.assertEqual(1, Account.objects.all().count(), "new user created even though no password entered")
 
     def test_usernameTaken(self):
-        response = self.client.post("/createaccounts.html", {"username": "user", "password": "password"})
+        response = self.client.post("account/create.html", {"username": "user", "password": "password"})
         self.assertEqual("username 'user' is already in use", response.context["message"], "username already taken")
 
     def test_noNewDuplicate(self):
-        self.client.post("/createaccounts.html", {"username": "user", "password": "password"})
+        self.client.post("account/create.html", {"username": "user", "password": "password"})
         self.assertEqual(1, Account.objects.all().count(), "new user created even though duplicate username entered")
-
--- TA_Scheduler/views/account/create.py
+++ TA_Scheduler/views/account/create.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
 from django.views import View
+from TA_Scheduler.models import Account
+from TA_Scheduler.utilities import AccountUtil
 
 
-class CreateAccount(View):
+class CreateAccounts(View):
     def get(self, request):
-        return render(request, "account/create.html")
+        return render(request, "account/create.html", {"message": "", "authorities": Account.AUTHORITY})
+
+    def post(self, request):
+        preexisting = True
+        username = request.POST['username']
+        try:
+            Account.objects.get(username=username)
+        except:
+            preexisting = False
+        if preexisting:
+            return render(request, "account/create.html", {"message": "username '" + username + "' is already in use",
+                                                           "authorities": Account.AUTHORITY})
+        else:
+            m = AccountUtil.createAccount(username, request.POST['password'], request.POST['authority'])
+            m.save()
+            # should I stay here or go somewhere else?
+            return render(request, "account/create.html", {"message": "account '" + username + "' created",
+                                                           "authorities": Account.AUTHORITY})
