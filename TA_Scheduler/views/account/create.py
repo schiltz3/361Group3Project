140d02a2a6008bc2d2cab8df03159daab20a2667
John Schiltz <johnschiltz2000@gmail.com>
2021-12-05 21:27:16 -0600

    Changes i made

--- TA_Scheduler/views/account/create.py
+++ TA_Scheduler/views/account/create.py
@@ -6,7 +6,7 @@ from TA_Scheduler.utilities.AccountUtil import AccountUtil
 
 class CreateAccount(View):
     def get(self, request):
-        return render(request, "account/create.html", {"message": " "})
+        return render(request, "account/create.html", {"message": " "}, )
 
     def post(self, request):
         preexisting = True
@@ -16,17 +16,19 @@ class CreateAccount(View):
         except:
             preexisting = False
         if preexisting:
-            return render(request, "account/create.html", {"message": "username '" + username + "' is already in use"})
+            return render(request, "account/create.html",
+                          {"message": "username '" + username + "' is already in use"}, )
         else:
             usertype = int(request.POST['authority'])
+
             if usertype == 1:
                 AccountUtil.createAdminAccount(username, request.POST['password'])
-                return render(request, "account/create.html", {"message": "account '" + username + "' created"})
+                return render(request, "account/create.html", {"message": "account '" + username + "' created"}, )
             elif usertype == 2:
                 AccountUtil.createInstructorAccount(username, request.POST['password'])
-                return render(request, "account/create.html", {"message": "account '" + username + "' created"})
+                return render(request, "account/create.html", {"message": "account '" + username + "' created"}, )
             elif usertype == 3:
                 AccountUtil.createTAAccount(username, request.POST['password'])
-                return render(request, "account/create.html", {"message": "account '" + username + "' created"})
+                return render(request, "account/create.html", {"message": "account '" + username + "' created"}, )
             else:
-                return render(request, "account/create.html", {"message": "enter user type"})
+                return render(request, "account/create.html", {"message": "enter user type"}, )
