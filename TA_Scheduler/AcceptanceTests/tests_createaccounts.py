897562ba17d0ec7204faa07277306fd9d24706f8
John Schiltz <45466247+schiltz3@users.noreply.github.com>
2021-12-06 22:02:47 +0000

    Automated formating

--- TA_Scheduler/AcceptanceTests/tests_createaccounts.py
+++ TA_Scheduler/AcceptanceTests/tests_createaccounts.py
@@ -14,47 +14,96 @@ class NewAccountTest(TestCase):
         # self.client.post("/testhome.html", select button? to go to createaccounts)
 
     def test_admin(self):
-        response = self.client.post("account/create.html", {"username": "new", "password": "password",
-                                                            "authority": "1", "message": "test"})
-        self.assertEqual("Administrator account 'new' created", response.context.get("message"),
-                         "admin confirmation message not given")
+        response = self.client.post(
+            "account/create.html",
+            {
+                "username": "new",
+                "password": "password",
+                "authority": "1",
+                "message": "test",
+            },
+        )
+        self.assertEqual(
+            "Administrator account 'new' created",
+            response.context.get("message"),
+            "admin confirmation message not given",
+        )
 
     def test_instructor(self):
-        response = self.client.post("account/create.html", {"username": "new", "password": "password",
-                                                            "authority": "2"})
-        self.assertEqual("Instructor account 'new' created", response.context.get("message"),
-                         "instructor confirmation message not given")
+        response = self.client.post(
+            "account/create.html",
+            {"username": "new", "password": "password", "authority": "2"},
+        )
+        self.assertEqual(
+            "Instructor account 'new' created",
+            response.context.get("message"),
+            "instructor confirmation message not given",
+        )
 
     def test_ta(self):
-        response = self.client.post("account/create.html", {"username": "new", "password": "password",
-                                                            "authority": "3"})
-        self.assertEqual("TA account 'new' created", response.context.get("message"),
-                         "ta confirmation message not given")
+        response = self.client.post(
+            "account/create.html",
+            {"username": "new", "password": "password", "authority": "3"},
+        )
+        self.assertEqual(
+            "TA account 'new' created",
+            response.context.get("message"),
+            "ta confirmation message not given",
+        )
 
     def test_inDatabase(self):
-        self.client.post("account/create.html", {"username": "new", "password": "password",
-                                                 "authority": "1"})
+        self.client.post(
+            "account/create.html",
+            {"username": "new", "password": "password", "authority": "1"},
+        )
         self.assertNotEqual(None, AccountUtil.getAccountByUsername("new"))
 
     def test_noUsername(self):
-        self.client.post("account/create.html", {"username": "", "password": "password", "authority": "1"})
-        self.assertEqual(1, (AccountUtil.getAllAccounts()).count(), "new user created even though no username entered")
+        self.client.post(
+            "account/create.html",
+            {"username": "", "password": "password", "authority": "1"},
+        )
+        self.assertEqual(
+            1,
+            (AccountUtil.getAllAccounts()).count(),
+            "new user created even though no username entered",
+        )
 
     def test_noPassword(self):
-        self.client.post("account/create.html", {"username": "new", "password": "", "authority": "1"})
-        self.assertEqual(1, (AccountUtil.getAllAccounts()).count(), "new user created even though no password entered")
+        self.client.post(
+            "account/create.html", {"username": "new", "password": "", "authority": "1"}
+        )
+        self.assertEqual(
+            1,
+            (AccountUtil.getAllAccounts()).count(),
+            "new user created even though no password entered",
+        )
 
     def test_usernameTaken(self):
-        response = self.client.post("account/create.html", {"username": "user", "password": "password",
-                                                            "authority": "1"})
-        self.assertEqual("username 'user' is already in use", response.context.get("message"),
-                         "username already taken")
+        response = self.client.post(
+            "account/create.html",
+            {"username": "user", "password": "password", "authority": "1"},
+        )
+        self.assertEqual(
+            "username 'user' is already in use",
+            response.context.get("message"),
+            "username already taken",
+        )
 
     def test_noNewDuplicate(self):
-        self.client.post("account/create.html", {"username": "user", "password": "password", "authority": "1"})
-        self.assertEqual(1, Account.objects.all().count(), "new user created even though duplicate username entered")
+        self.client.post(
+            "account/create.html",
+            {"username": "user", "password": "password", "authority": "1"},
+        )
+        self.assertEqual(
+            1,
+            Account.objects.all().count(),
+            "new user created even though duplicate username entered",
+        )
 
     def test_noAuthority(self):
-        response = self.client.post("account/create.html", {"username": "user", "password": "password",
-                                                            "authority": ""})
+        response = self.client.post(
+            "account/create.html",
+            {"username": "user", "password": "password", "authority": ""},
+        )
         self.assertEqual("enter user type", response.context.get("message"))
