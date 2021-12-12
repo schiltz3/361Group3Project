923ca5aed43b5983e6e92008b4c7ad6510a7414c
lisa <lis.lisaeva@gmail.com>
2021-12-12 00:45:57 -0600

    create HTML, add get method to view

--- TA_Scheduler/views/course/delete.py
+++ TA_Scheduler/views/course/delete.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
+from TA_Scheduler.utilities.CourseUtil import CourseUtil
 
 
 class DeleteCourse(View):
+
     def get(self, request):
-        return render(request, "course/delete.html")
+
+        if request.user.is_anonymous:
+            return redirect("/", {"error": "User is not authorized to delete courses"})
+        elif request.user.groups.filter(name="instructor").exists():
+            return redirect(
+                "/dashboard/instructor/",
+                {"error": "Instructors are not authorized to delete courses"},
+            )
+        elif request.user.groups.filter(name="ta").exists():
+            return redirect(
+                "/dashboard/ta/", {"error": "TAs are not authorized to delete courses"}
+            )
+
+        courses = CourseUtil.getAllCourses()
+        return render(request, "course/delete.html", {"courses" : courses})
+
+    
