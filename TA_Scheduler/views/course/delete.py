78b067a81bc666cc9f7c661734602e1729b90a66
John Schiltz <johnschiltz2000@gmail.com>
2021-12-12 03:52:04 -0600

    put doc on first line

--- TA_Scheduler/views/course/delete.py
+++ TA_Scheduler/views/course/delete.py
@@ -30,6 +30,7 @@ class DeleteCourse(View):
 
     def post(self, request):
         """Called when the user clicks the action to delete selected courses.
+
         @param request: Request from course/delete.html
         @return: Response with remaining courses, and a message
             to inform about success or an error
